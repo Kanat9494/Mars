f2cbf279ad1f39777ddd878a00b47e3bd3474c70
Kanat9494 <kkanat94@gmail.com>
2024-01-19 00:24:09 +0600

    12

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mars/second.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -77,49 +81,472 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 115.0,
+            floating: true,
+            backgroundColor: Colors.transparent,
+            // backgroundColor:
+            //     const Color.fromARGB(255, 22, 9, 206), // Розовый фон
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20.0), // Нижний левый угол
+                bottomRight: Radius.circular(20.0), // Нижний правый угол
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            flexibleSpace: FlexibleSpaceBar(
+              titlePadding: const EdgeInsets.only(left: 0, right: 0),
+             centerTitle: true,
+              title: Container(
+                decoration: const BoxDecoration(
+              color:  Color.fromARGB(255, 22, 9, 206),
+              borderRadius: BorderRadius.only(
+                bottomLeft:
+                    Radius.circular(20), // Закругление левого нижнего угла
+                bottomRight:
+                    Radius.circular(20), // Закругление правого нижнего угла
+              ),
             ),
-          ],
-        ),
+
+               
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 70.0,
+                  ),
+                  const Text(
+                    'Текст по середине',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 6.0),
+                    height: 35.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Поиск',
+                        hintStyle: TextStyle(fontSize: 14.0),
+                        border: InputBorder.none,
+                        icon: Icon(Icons.search),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              )
+            ),
+          ),
+
+          SliverFixedExtentList(
+            itemExtent: 225,
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return SizedBox(
+                  height: 225,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 100,
+                    itemBuilder: (BuildContext context, int innerIndex) {
+                      // print('First row $innerIndex');
+        final item = popularItems[index % popularItems.length];
+        return PopularItemCard(item: item);
+                    }
+                  ),
+                );
+              },
+              childCount: 1,
+            ),
+
+),
+          const SliverToBoxAdapter(child: const SizedBox(height: 10.0),),
+          SliverPadding(
+  padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+  sliver: SliverGrid(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      childAspectRatio: 3 / 5,
+      mainAxisSpacing: 10.0,
+      crossAxisSpacing: 5.0,
+      mainAxisExtent: 355.0
+    ),
+    delegate: SliverChildBuilderDelegate(
+      (BuildContext context, int index) {
+        // print(index);
+        //final itemIndex = index % allItems3.length; // Handle index overflow
+        final item = allItems3[index];
+      
+        return MarketplaceItemCard(item: item);
+      },
+      childCount: 1000, // Display 1000 elements
+    ),
+  ),
+  
+),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+     floatingActionButton:  Container(
+          width: double.infinity, // Растягиваем на всю ширину
+          padding: EdgeInsets.symmetric(horizontal: 16.0), // Произвольные отступы по бокам
+          child: FloatingActionButton(
+            onPressed: () {
+              // Handle button press
+            },
+            backgroundColor: const Color.fromARGB(255, 22, 9, 206),
+            tooltip: 'Increment',
+            child: const Text('В корзину', style: TextStyle(color: Colors.white, fontSize: 18)),
+          ),),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
+
+
+class MarketplaceItemCard extends StatelessWidget {
+  final Item item;
+
+  const MarketplaceItemCard({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+//     return Container(
+//   margin: const EdgeInsets.all(3.0),
+//   decoration: BoxDecoration(
+//             color: Colors.grey[200], borderRadius: BorderRadius.circular(10.0)),
+//   child: Stack(
+//     children: [
+//       Column(
+//           children: <Widget>[
+//             AspectRatio(
+//               aspectRatio: 1.0,
+             
+//               child: ClipRRect(
+//                 borderRadius: BorderRadius.circular(10.0),
+//                 child: CachedNetworkImage(
+//                   imageUrl: item.imageUrl,
+//                   fit: BoxFit.cover,
+//                 ),
+//               ),
+              
+//             ),
+//             const SizedBox(height: 8.0),
+//             Text(
+//               item.name,
+//               style: const TextStyle(
+//                 fontSize: 18,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             const SizedBox(height: 4.0),
+//             Text(
+//               item.category ?? "Нет",
+//               style: TextStyle(
+//                 fontSize: 14,
+//                 color: Colors.grey[600],
+//               ),
+//             ),
+//             const SizedBox(height: 4.0),
+//             Text(
+//               item.additionalInfo ?? "Нет",
+//               style: TextStyle(
+//                 fontSize: 14,
+//                 color: Colors.grey[600],
+//               ),
+//             ),
+//           ],
+//         ),
+
+//          Positioned.fill(
+//               child: Material(
+//                 color: Colors.transparent,
+//                 child: InkWell(
+//                   borderRadius: BorderRadius.circular(10.0),
+//                   splashColor:
+//                       Colors.grey.withOpacity(0.4), // Цвет волны при нажатии
+//                   highlightColor: Colors.grey.withOpacity(0.3),
+//                   splashFactory: InkRipple.splashFactory,
+//                   onTap: () {
+//                     Future.delayed(Duration.zero, () {
+//                       if (!context.mounted) return;
+//                       Navigator.push(
+//                         context,
+//                         PageRouteBuilder(
+//                           pageBuilder: (context, animation1, animation2) =>
+//                               const SliverPage(),
+//                           transitionDuration: Duration.zero,
+//                           reverseTransitionDuration: Duration.zero,
+//                         ),
+//                       );
+//                     });
+//                   },
+//                 ),
+//               ),
+//             )
+//     ],
+//   ),
+// );
+
+
+return Container(
+  margin: const EdgeInsets.all(3.0),
+  decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0)),
+  child: Stack(
+    children: [
+      Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 0.8,
+             
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: CachedNetworkImage(
+                  imageUrl: item.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              item.name,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 4.0),
+            Text(
+              item.category ?? "Нет",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[600],
+              ),
+            ),
+            const SizedBox(height: 4.0),
+            Text(
+              item.additionalInfo ?? "Нет",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[600],
+              ),
+            ),
+            MaterialButton(
+  onPressed: () {},
+  color: const Color.fromARGB(255, 22, 9, 206),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(10.0), // Указываете радиус закругления
+  ),
+  child: const Text('В корзину', style: TextStyle(color: Colors.white)),
+)
+          ],
+        ),
+
+         Positioned.fill(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(10.0),
+                  splashColor:
+                      Colors.grey.withOpacity(0.4), // Цвет волны при нажатии
+                  highlightColor: Colors.grey.withOpacity(0.3),
+                  splashFactory: InkRipple.splashFactory,
+                  onTap: () {
+
+                    // Future.delayed(Duration.zero, () {
+                    //   if (!context.mounted) return;
+                    //   Navigator.push(
+                    //     context,
+                    //     PageRouteBuilder(
+                    //       pageBuilder: (context, animation1, animation2) =>
+                    //           const SliverPage(),
+                    //       transitionDuration: Duration.zero,
+                    //       reverseTransitionDuration: Duration.zero,
+                    //     ),
+                    //   );
+                    // });
+                    Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (context, animation1, animation2) =>
+                              const SliverPage(),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero,
+                        ),
+                      );
+                  },
+                ),
+              ),
+            )
+    ],
+  ),
+);
+  }
+}
+
+class Item {
+  final String imageUrl;
+  final String name;
+  final String? category; // Made 'category' parameter optional with '?'
+  final String? additionalInfo; // Made 'additionalInfo' parameter optional with '?'
+  final String price;
+
+  Item({
+    required this.imageUrl,
+    required this.name,
+    this.category, // Updated to make it optional
+    this.additionalInfo, // Updated to make it optional
+    required this.price,
+  });
+}
+
+
+final List<Item> allItems3 = List.generate(
+  1000,
+  (index) => Item(
+    imageUrl: 'https://picsum.photos/id/${index % 10 + 1}/200/300',
+    name: 'Item $index',
+    category: 'Category $index',
+    additionalInfo: 'Info $index',
+    price: '\$${index + 1}',
+  ),
+);
+
+
+class PopularItemCard extends StatelessWidget {
+  final Item item;
+
+  const PopularItemCard({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Material(
+        borderRadius: BorderRadius.circular(10.0),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+    color: Colors.transparent,
+
+        child: InkResponse(
+          splashFactory: InkRipple.splashFactory,
+          containedInkWell: true,
+          onTapDown: (details){},
+        onTap: (){},
+        // borderRadius: BorderRadius.circular(10.0),
+        
+        child: Column(
+        children: <Widget>[
+            Ink.image(
+              image: NetworkImage(
+              item.imageUrl,
+            ),
+
+
+            
+            height: 120,
+              width: 120,
+              fit: BoxFit.cover,
+            ),
+          const SizedBox(height: 16.0),
+          Text(
+            item.name,
+            style: const TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white, // Изменение цвета текста на белый
+            ),
+          ),
+          const SizedBox(height: 4.0),
+          Text(
+            item.price,
+            style: const TextStyle(
+              fontSize: 14.0,
+              color: Colors.white, // Изменение цвета текста на белый
+            ),
+          ),
+          const SizedBox(height: 16.0),
+        ],
+      ),
+      ),
+
+//       child: InkWell(
+//   onTap: () {
+//     // Обработчик при касании (поднимите соответствующий код обработки здесь)
+//     print('Tapped');
+//   },
+//   onTapDown: (details) {
+//     // Обработчик при касании экрана
+//     print('Tapped Down');
+//     // Вызовите свой код обработки, если это необходимо
+//   },
+//   highlightColor: Colors.transparent, // Устанавливаем прозрачный цвет ряби
+//   child: Column(
+//     children: <Widget>[
+//       Ink.image(
+//         image: NetworkImage(item.imageUrl),
+//         height: 120,
+//         width: 120,
+//         fit: BoxFit.cover,
+//       ),
+//       const SizedBox(height: 16.0),
+//       Text(
+//         item.name,
+//         style: const TextStyle(
+//           fontSize: 16.0,
+//           fontWeight: FontWeight.bold,
+//           color: Colors.white,
+//         ),
+//       ),
+//       const SizedBox(height: 4.0),
+//       Text(
+//         item.price,
+//         style: const TextStyle(
+//           fontSize: 14.0,
+//           color: Colors.white,
+//         ),
+//       ),
+//     ],
+//   ),
+// )
+
+
+
+
+
+
+      )
+    );
+  }
+}
+
+
+final List<Item> popularItems = [
+  Item(
+    imageUrl: 'https://picsum.photos/id/1/200/300',
+    name: 'Item 1',
+    price: '\$10',
+  ),
+  Item(
+    imageUrl: 'https://picsum.photos/id/2/200/300',
+    name: 'Item 2',
+    price: '\$15',
+  ),
+  Item(
+    imageUrl: 'https://picsum.photos/id/3/200/300',
+    name: 'Item 3',
+    price: '\$20',
+  ),
+];
